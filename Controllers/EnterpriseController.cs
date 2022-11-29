b36c3afc54ac9e66375103e2f5a1f2e505265ee0
Shigeru <proshigeru@gmail.com>
2022-11-28 22:39:04 -0300

    first commit

---
+++ Controllers/EnterpriseController.cs
@@ -0,0 +1,169 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PI.Application.Intefaces;
+using PI.Domain.ViewModel.Enterpriqse;
+using PI.Domain.ViewModel.Enterprise;
+using PI.Domain.ViewModel.UserSupport;
+
+namespace PI.Web.Controllers
+{
+    public class EnterpriseController : Controller
+    {
+        #region Properties
+        private IEnterpriseAppService _enterpriseService;
+        private readonly IUserSupportAppService _userSupportService;
+        private readonly IUserProblemsCategoryAppService _userProblemsCategoryService;
+        #endregion
+
+        #region DIP
+        public EnterpriseController(IEnterpriseAppService enterpriseService, IUserSupportAppService userSupportService, IUserProblemsCategoryAppService userProblemsCategoryService)
+        {
+            _enterpriseService = enterpriseService;
+            _userSupportService = userSupportService;
+            _userProblemsCategoryService = userProblemsCategoryService;
+        }
+        #endregion
+
+        #region Actions 
+
+        [HttpGet]
+        public async Task<IActionResult> Home()
+        {
+            string enterpriseCnpj = Request.Cookies["enterpriseCnpj"].ToString();
+            AmountOfMachinesStatusByEnterpriseViewModel model = await _enterpriseService.GetMachinesStatusByEnterpriseCnpj(enterpriseCnpj);
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Settings()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(string enterpriseForRegisterAsString)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    CreateEnterpriseViewModel? create = JsonConvert.DeserializeObject<CreateEnterpriseViewModel>(enterpriseForRegisterAsString);
+
+                    if (create == null)
+                    {
+                        TempData["message"] = "Ocorreu um erro para criar um cadastro tente novamente mais tarde , ou entre em contato !";
+
+                        return View();
+                    }
+
+                    bool isValid = await _enterpriseService.SignUp(create);
+
+                    if (isValid)
+                    {
+                        return View("CreateEnterpriseSuccessed");
+                    }
+                    ModelState.AddModelError(String.Empty, TempData["message"].ToString());
+
+                    return View();
+                }
+                catch
+                {
+                    TempData["message"] = "Não foi possível criar entre em contato com o administrador";
+                    return View();
+                }
+            }
+
+            return View();
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ViewProfie()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateEnterprise(UpdateEnterpriseViewModel model)
+        {
+            bool isUpdated = await _enterpriseService.Update(model);
+
+            TempData["message"] = isUpdated ? "Seus dados foram atualizados com sucesso !" : "Não foi possível fazer a alteração de seus dados tente novamente mais tarde ou mande um recado na tela de suporte";
+
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(EnterpriseLoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isLogin = await _enterpriseService.Login(login);
+                if (isLogin)
+                {
+                    TempData["message"] = "Usuario adicionado com sucesso!";
+                    Response.Cookies.Append("enterpriseCnpj", login.Login);
+                    return RedirectToAction("Home");
+                }
+
+                ModelState.AddModelError(nameof(login.Password), "Senha ou usuário incorretos");
+
+                return View();
+            }
+            TempData["message"] = "Senha ou usuario invalidos";
+            return View(login);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Support()
+        {
+
+            //string enterpriseCnpjAsString = Request.Cookies["enterpriseCnpj"].ToString();
+
+            //EnterpriseProfileViewModel model = await _enterpriseService.GetEnterpriseByCnpj(enterpriseCnpjAsString);
+
+            //EnterpriseProfileViewModel model = await _enterpriseService.GetEnterpriseForProfileById(enterpriseId);
+
+            ViewBag.userProblemsCategoriesAsString = await _userProblemsCategoryService.GetNameOfAllAsString();
+
+            AddUserSupportViewModel modelForView = new AddUserSupportViewModel() { EnterpriseId = 2, NameOfEnterprise = "Rolo doido produções" };
+
+            return View(modelForView);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Support(AddUserSupportViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isCreated = await _userSupportService.CreateUserReport(model);
+
+                if (isCreated)
+                {
+                    TempData["message"] = "Relato enviado com sucesso!";
+                    return View();
+                }
+                TempData["message"] = "Näo foi possível adicionar esse relato , tente novamente mais tarde ou entre em contato aocm o administrador";
+
+                return View(); // see what's pages will be recieve this content about this problem 
+            }
+
+            return View(model);
+        }
+        #endregion
+
+    }
+}
---
+++ Controllers/MachineController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PI.Application.Intefaces;
+using PI.Domain.ViewModel.Enterpriqse;
+using PI.Domain.ViewModel.Machine;
+
+namespace PI.Web.Controllers;
+
+public class MachineController : Controller
+{
+    #region Properties
+
+    private readonly IMachineAppService _machineService;
+    private readonly IMachineCategoryAppService _machineCategoryService;
+
+    #endregion
+
+    #region DIP 
+
+    public MachineController(IMachineAppService machineService, IMachineCategoryAppService machineCategoryService)
+    {
+        _machineService = machineService;
+        _machineCategoryService = machineCategoryService;
+    }
+
+    #endregion
+
+    #region Actions
+
+    [HttpGet]
+    public async Task<IActionResult> List()
+    {
+        string enterpriseOfCurrentSessionCnpj = Request.Cookies["enterpriseCnpj"];
+
+        List<MachineForListViewModel> model = await _machineService.GetMachinesByEnterpriseCnpj(enterpriseOfCurrentSessionCnpj);
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Add()
+    {
+        ViewBag.MachineCategoriesAsStringList = await _machineCategoryService.GetNameOfCategoriesAsString();
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add(AddMachineViewModel model)
+    {
+        string? enterpriseCnpj = Request.Cookies["enterpriseCnpj"].ToString();
+        bool isAdded = await _machineService.Add(model, enterpriseCnpj);
+
+        TempData["message"] = isAdded ? "Máquian adicionada com sucesso!!"
+            : "Occorreu um erro tente novamente mais tarde ou entre em contato com o administrador";
+
+        return View("Enterprise", "Home");
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Edit(int id)
+    {
+        EditMachineViewModel modelForEditDataOfMachine = await _machineService.GetMachineDataForEdit(id);
+
+        TempData["categories"] = await _machineCategoryService.GetNameOfCategoriesAsString();
+
+        return View(modelForEditDataOfMachine);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Edit(EditMachineViewModel model)
+    {
+        bool isUpdated = await _machineService.UpdateMachine(model);
+
+        TempData["message"] = isUpdated ? "Máquina atualizada com sucesso!" :
+            "Occoreu um erro tente novamente mais tarde ou envie uma mensagem para a nossa equipe";
+
+        return RedirectToAction("List");
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete(int id)
+    {
+        bool isDeleted = await _machineService.RemoveMachine(id);
+
+        TempData["message"] = isDeleted ? "Máquina apagada com sucesso!" :
+            "Não foi possível deletar a máquina ,tente novamente mais tarde ou nos envie uma mensagem na tela de suporte";
+
+        return RedirectToAction("List");
+    }
+    public async Task<IActionResult> SendDataFromSensors([FromBody] MachineDataRecieveFromSensorsJsonModel model)
+    {
+        bool isRegisted = await _machineService.AddRegisterOfMachineFromJson(model);
+
+        return isRegisted ? Ok("Added with success!") : BadRequest("Register dosen't added with expected , contact the administrator for more details");
+    }
+
+    public async Task<IActionResult> DownloadLogDataOfMachine(int id)
+    {
+        string dataAsCsv = await _machineService.GetHistoryDataByCsvByMachineId(id);
+        EditMachineViewModel machineForEdit = await _machineService.GetMachineDataForEdit(id);
+
+        return File(System.Text.Encoding.UTF8.GetBytes(dataAsCsv), "text/csv", "data_da_maquina_" + machineForEdit.Model + ".csv");
+    }
+    [HttpGet]
+    public async Task<IActionResult> DetailsAboutMachine(int id)
+    {
+        MachineDetailsViewModel model = await _machineService.GetMachineForDetails(id);
+
+        return View(model);
+    }
+
+    [HttpGet]
+    public async Task<JsonResult> DetailsAboutMachineAjaxHandler(int id, string status)
+    {
+        string result = await _machineService.GetJsonForDetailsAboutMachineAjaxHandler(id, status);
+
+        return Json(result);
+    }
+
+    #endregion
+}
